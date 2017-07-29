524143800548b8b4e85c7694161fd5978ee66a6e
alexc <alex.jj.cameron@gmail.com>
2017-07-29 10:01:05 +1000

    Head script will now attempt to ignore errors and record which images caused them

--- run_rms.py
+++ run_rms.py
@@ -16,45 +16,51 @@ field_data = config.full_filename_list(field_data)
 for field in field_data:
     for band in field_data[field]['bands']:
         flags = ''
-        print "\n\n****************\n****************\nField %s, band %s : \n" % (field, band)
-        if not isfile(field_data[field][band]['rms_crude']):
-            print "Making initial RMS map..."
-            rms.wht_to_rms(field_data[field][band]['wht'], field_data[field][band]['rms_crude'], zero_handle=config.config_dict['wht_zero'])
-        else:
-            print "First pass RMS map for field %s band %s already exists!" % (field, band)
+        try:
+            print "\n\n****************\n****************\nField %s, band %s : \n" % (field, band)
+            if not isfile(field_data[field][band]['rms_crude']):
+                print "Making initial RMS map..."
+                rms.wht_to_rms(field_data[field][band]['wht'], field_data[field][band]['rms_crude'], zero_handle=config.config_dict['wht_zero'])
+            else:
+                print "First pass RMS map for field %s band %s already exists!" % (field, band)
 
-        print "SExtracting...\n"
-        rms.crude_SExtract(field_data[field][band])
+            print "SExtracting...\n"
+            rms.crude_SExtract(field_data[field][band])
 
-        if not isfile(field_data[field][band]['false_img']):
-            print "Generating false sources..."
-            noise.false_sources(field_data[field][band], no_sources=field_data[field][band]['no_false_srcs'])
-        else:
-            print "False sources image for field %s band %s already exists!" % (field, band)
+            if not isfile(field_data[field][band]['false_img']):
+                print "Generating false sources..."
+                noise.false_sources(field_data[field][band], no_sources=field_data[field][band]['no_false_srcs'])
+            else:
+                print "False sources image for field %s band %s already exists!" % (field, band)
 
-        print "SExtracting false sources...\n"
-        noise.false_SExtract(field_data[field][band])
+            print "SExtracting false sources...\n"
+            noise.false_SExtract(field_data[field][band])
 
-        print "Calculating normalisation constant..."
-        norm_constant, fal_src_count = noise.rms_norm_constant(field_data[field][band]['cat_false'])
+            norm_constant, fal_src_count = noise.rms_norm_constant(field_data[field][band]['cat_false'])
+            print "%d false sources SExtracted.\n" % fal_src_count
+            print "Calculating normalisation constant..."
 
-        if not fal_src_count == field_data[field][band]['no_false_srcs']:
-            flags = flags + ' falsecount '
+            if not fal_src_count == field_data[field][band]['no_false_srcs']:
+                flags = flags + ' falsecount '
 
-        print "Creating normalised RMS map..."
-        if isfile(field_data[field][band]['rms_norm']):
-            remove(field_data[field][band]['rms_norm'])
-        rms.norm_rms_map(field_data[field][band]['rms_crude'], field_data[field][band]['rms_norm'], norm_constant)
+            print "Creating normalised RMS map..."
+            if isfile(field_data[field][band]['rms_norm']):
+                remove(field_data[field][band]['rms_norm'])
+            rms.norm_rms_map(field_data[field][band]['rms_crude'], field_data[field][band]['rms_norm'], norm_constant)
 
-        noise.test_SExtract(field_data[field][band])
-        test_norm, test_count = noise.rms_norm_constant('test.cat')
+            noise.test_SExtract(field_data[field][band])
+            test_norm, test_count = noise.rms_norm_constant('test.cat')
 
-        print "noise_measured / noise_estimated = ", test_norm
+            print "noise_measured / noise_estimated = ", test_norm
 
-        if not 0.99 < test_norm < 1.001:
-            flags = flags + ' normacc:' + str(test_norm) + ' '
+            if not 0.99 < test_norm < 1.001:
+                flags = flags + ' normacc:' + str(test_norm) + ' '
 
-        if not flags == '':
+            if not flags == '':
+                flagged_imgs.append(field+'_'+band+flags)
+        except IOError:
+            print 'IOError!!'
+            flags = flags + ' ERROR:IOError '
             flagged_imgs.append(field+'_'+band+flags)
 
 # Clean up
